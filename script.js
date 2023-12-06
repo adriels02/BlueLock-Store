32e48dc7c37ad0d9aa4f2aac9d8fca7f03b7b031
Adriel <adriel_leite12@outlook.com>
2023-12-05 22:15:18 -0300

    finalizando responsividade e adição da página contas

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector(".menu-button");
+
+
+menuButton.addEventListener("click", () => {
+    navbar.classList.toggle("show-menu");
+});
+
+const isSegundaPagina = document.body.classList.contains('pagina um');
+
+
 let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval(function(){
-nextImage()
+nextImage()    
 }, 5000)
 
 function nextImage(){
     count++
     if(count > 3){
         count = 1
-    }
+    }    
     document.getElementById("radio" + count).checked = true;
-}
-
-    const navbar = document.querySelector(".navbar");
-    const menuButton = document.querySelector(".menu-button");
-
-    menuButton.addEventListener("click", () => {
-        navbar.classList.toggle("show-menu");
-    });
+}    
 
 
+    document.getElementById('linkParaOFinal').addEventListener('click', function() {
+       
+        window.scrollTo(0, document.body.scrollHeight);
+      });
